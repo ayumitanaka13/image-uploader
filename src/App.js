746065aea55621413c1a78ea7c8dd5b19e853a28
ayumitanaka <tanakayumi13@gmail.com>
2021-04-11 22:26:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firebase, { storage } from "./firebase/firebase";
 
 function App() {
+  // styling
+  const styles = {
+    margin: 40,
+  };
+
+  // UseState
+  const [image, setImage] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  // event handler
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (image === "") {
+      console.log("Not selected");
+    }
+    const uploadTask = storage.ref(`/images/${image.name}`).put(image);
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, complete);
+  };
+
+  const onChange = (e) => {
+    const image = e.target.files[0];
+    setImage(image);
+  };
+
+  // function
+  const complete = () => {
+    storage
+      .ref("images")
+      .child(image.name)
+      .getDownloadURL()
+      .then((firebaseUrl) => {
+        setImageUrl(firebaseUrl);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles}>
+        <h1>React Image Uploader</h1>
+        <form onSubmit={onSubmit}>
+          <input type="file" onChange={onChange} />
+          <button>Upload</button>
+        </form>
+        <img src={imageUrl} alt="uploaded" width="300" height="300" />
+      </div>
     </div>
   );
 }
